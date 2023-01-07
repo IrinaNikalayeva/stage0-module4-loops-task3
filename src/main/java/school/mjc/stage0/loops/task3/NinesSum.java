27ad8fd3eaf86c80383d789454608328ac70e377
Iryna_Nikalayeva <irinnik92@gmail.com>
2023-01-07 21:44:43 +0100

    Performed NinesSum task

--- src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
@@ -2,6 +2,14 @@ package school.mjc.stage0.loops.task3;
 
 public class NinesSum {
     public void calculateSum(int lengthOfLastNumber) {
-
+        int sum = 0;
+        for (int i = 1; i <= lengthOfLastNumber ; i++) {
+            String tempNum = "";
+            for (int j = 0; j < i; j++) {
+                tempNum += "9";
+            }
+            sum += Integer.parseInt(tempNum);
+        }
+        System.out.println(sum);
     }
 }
