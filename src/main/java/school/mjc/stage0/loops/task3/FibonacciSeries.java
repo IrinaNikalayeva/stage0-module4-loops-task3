92f5bb98de8f5eff8ae2b792970ca774b55d644a
Iryna_Nikalayeva <irinnik92@gmail.com>
2023-01-07 22:30:31 +0100

    Performed FibonacciSeries task

--- src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
+++ src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
@@ -2,5 +2,16 @@ package school.mjc.stage0.loops.task3;
 
 public class FibonacciSeries {
     public void printFibonacci(int lastFibonacci) {
+        int prevFib = 0;
+        int nextFib = 1;
+        System.out.println(prevFib);
+        System.out.println(nextFib);
+        for (int i = 3; i <= lastFibonacci; i++) {
+            int fibonacciNum = prevFib + nextFib;
+            System.out.println(fibonacciNum);
+            prevFib = nextFib;
+            nextFib = fibonacciNum;
+
+        }
     }
 }
