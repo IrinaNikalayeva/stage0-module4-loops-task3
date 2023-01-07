3976c19dcd1c0be56624f8abadb801801d8b5e76
Iryna_Nikalayeva <irinnik92@gmail.com>
2023-01-07 21:30:55 +0100

    Performed DigitsSum task

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -1,7 +1,17 @@
 package school.mjc.stage0.loops.task3;
 
 public class DigitsSum {
-    public void printDigitsSum(int t){
-
+    public void printDigitsSum(int t) {
+        if (t < 0) {
+            t = t * -1;
+        }
+        int numberLength = (t + "").length();
+        int sum = 0;
+        for (int i = 0; i <= numberLength; i++) {
+            int tempNum = (int) (t % Math.pow(10, numberLength - i));
+            int digit = (int) (tempNum / Math.pow(10, numberLength - 1 - i));
+            sum += digit;
+        }
+        System.out.println(sum);
     }
 }
